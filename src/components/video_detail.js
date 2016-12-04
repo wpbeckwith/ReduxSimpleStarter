ff7aad6e9a7086d7d1fabebf9209c1c9ad2905ad
Wendell Beckwith <wendell.beckwith@ca.com>
2016-12-04 00:05:26 -0600

    Completed youtube integration.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,3 +1,24 @@
-/**
- * Created by wbeckwith on 12/2/16.
- */
+import React from 'react';
+
+const VideDetail = ({video}) => {
+    if (!video) {
+        return <div>Loading ...</div>;
+    }
+
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-response-item" src={url}></iframe>
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+        </div>
+    );
+}
+
+export default VideDetail;
